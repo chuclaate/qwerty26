e149eedfb9745e184005cd07f24d94c46f8098e4
chuclaate <chuclaate@gmail.com>
2024-09-21 10:04:10 +0500

    Delete qwerty26.js

--- qwerty26.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll('.product-card button');
-
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            alert('Product added to cart!');
-        });
-    });
-});
